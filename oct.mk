73262886941ab7dedd2d118d961fc5acc16e3226
Donald Hoskins <grommish@gmail.com>
2014-10-19 05:39:08 -0400

    Fix Derp

--- oct.mk
+++ oct.mk
@@ -4,6 +4,6 @@ $(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
 $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_phone.mk)
+$(call inherit-product, vendor/oct/config/common_phone.mk)
 
 PRODUCT_NAME := oct_d2lte
